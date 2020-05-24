9f8d179906d332d087f8b3aa6b61d9ecb550908f
Chaithanya <chaithanya.hr@instamojo.com>
2020-05-24 14:10:37 +0530

    Preserving the state

--- popup.js
+++ popup.js
@@ -2,15 +2,20 @@
 
 let stopButton = document.getElementById('stop-button');
 
-function setStyling (inputChecked) {
+function setStyling (isExtensionEnabled) {
   const switch1 = document.getElementsByClassName('switch')[0];
-  if (inputChecked) {
+  if (isExtensionEnabled) {
     switch1.style.backgroundColor = "#80c75f";
   }
   else {
     switch1.style.backgroundColor = "#ccc";
   }
 }
+
+chrome.storage.local.get(['isExtensionEnabled'], function(result) {
+  stopButton.checked = result.isExtensionEnabled;
+  setStyling(result.isExtensionEnabled);
+});
 
 stopButton.onclick = function(element) {
   const msg = {isExtensionEnabled: element.target.checked };
